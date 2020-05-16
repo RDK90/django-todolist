1115e9156949fea00b4db6e8184bc2ce3252730e
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-05-16 17:49:30 +0100

    Add tests for Todo API

--- todos/todo_api.py
+++ todos/todo_api.py
@@ -14,15 +14,16 @@ def get_all_todos(request):
         return Response(todo_serializer.data)
 
 @api_view(['GET'])
-def todos_by_id(request, id):
+def todos_by_id(request, todo_id):
     if request.method == "GET":
-        if type(id) == int:
-            todos = Todo.objects.filter(id=id).values()
+        try:
+            todo_id = int(todo_id)
+            todos = Todo.objects.filter(id=todo_id).values()
             if not todos:
-                content = {"Todo item with ID {} not found".format(id)}
+                content = {"Todo item with ID {} not found".format(todo_id)}
                 return Response(content, status=status.HTTP_404_NOT_FOUND)
             todo_serializer = TodoSerializer(todos, many=True)
             return Response(todo_serializer.data)
-        else:
-            content = {"{} is an invalid ID".format(id)}
+        except:
+            content = {"{} is an invalid ID".format(todo_id)}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
--- todos/urls.py
+++ todos/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views, todo_api
 
+app_name = "todos"
+
 urlpatterns = [
     path('todos/', views.index, name='index'),
     path('todos/all', todo_api.get_all_todos, name="get_all_todos"),
-    path('todos/<id>', todo_api.todos_by_id, name="todo_by_id"),
+    path('todos/<todo_id>', todo_api.todos_by_id, name="todo_by_id"),
     path('details/<id>', views.details),
     path('add/', views.add, name='add')
 ]
