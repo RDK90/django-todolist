0c4bd08bc3a1db763b3ea8a705e899e66923e9b2
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-05-17 11:32:51 +0100

    Removal of datetime in models and replacement with djangos timezone method

--- todos/models.py
+++ todos/models.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
-from datetime import datetime
+from django.utils import timezone
+import pytz
 
 from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
     def __str__(self):
         return self.title
